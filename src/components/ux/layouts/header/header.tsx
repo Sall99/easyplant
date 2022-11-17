2a6aed23303ceef0477bbb4ecc285a53c2bf7d76
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-11-17 22:18:57 +0000

    feat(components): add hero

--- src/components/ux/layouts/header/header.tsx
+++ src/components/ux/layouts/header/header.tsx
@@ -1,12 +1,17 @@
-import { Icon } from "components/ui";
+import { Icon, Button } from "components";
 import { Link } from "react-router-dom";
 import { navLinks } from "services";
 
 export const Header = () => {
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <header className="bg-hero-pattern h-_700 bg-cover bg-center">
       <div className="flex justify-between items-center text-sm font-normal leading-_17.26 text-gray-100 max-w-_1140 m-auto py-5">
-        <Icon name="Logo" />
+        <Link to="/">
+          <Icon name="Logo" />
+        </Link>
         <div>
           <ul className="flex items-center gap-5">
             {navLinks.map((link) => (
@@ -16,10 +21,20 @@ export const Header = () => {
             ))}
           </ul>
         </div>
-        <div className="flex items-center gap-2">
-          <Icon name="Cart" />
-          <Link to="/cart">Cart</Link>
-        </div>
+        <Link to="/cart" className="flex items-center gap-2">
+          <Icon name="Cart" /> <p>Cart</p>
+        </Link>
+      </div>
+      <div className="w-_592 m-auto mt-_132 flex flex-col justify-center items-center gap-9">
+        <p className=" text-_66 -tracking-_2 text-center font-semibold text-gray-200">
+          Grow beautiful <br /> self-watering plants
+        </p>
+        <Button
+          label="Shop plants"
+          type="button"
+          onClick={handleClick}
+          className="w-60"
+        />
       </div>
     </header>
   );
