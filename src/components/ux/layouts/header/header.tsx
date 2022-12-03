b99de8fa8d040bed4a62ea9d3e2d284473af995c
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-12-03 23:47:54 +0000

    feat(header): add animtion

--- src/components/ux/layouts/header/header.tsx
+++ src/components/ux/layouts/header/header.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import { IHeader } from "@types";
 import clsx from "clsx";
 import { Icon, Button } from "components";
-import { Link, useNavigate } from "react-router-dom";
+
 import { navLinks } from "services";
 import { publicRoutes } from "routes";
 
@@ -20,34 +22,67 @@ export const Header: FC<IHeader> = ({ hero = true }) => {
         hero ? "bg-hero-pattern h-_700 bg-cover bg-center " : ""
       )}
     >
-      <div className="flex justify-between items-center text-sm font-normal leading-_17.26 text-gray-100 max-w-_1140 m-auto py-5">
+      <motion.div
+        className="flex justify-between items-center text-sm font-normal leading-_17.26 text-gray-100 max-w-_1140 m-auto py-5"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <Link to="/">
-          <Icon name="Logo" />
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+            <Icon name="Logo" />
+          </motion.div>
         </Link>
         <div className="hidden sm:block">
           <ul className="flex items-center gap-5">
             {navLinks.map((link) => (
-              <li key={link.name}>
+              <motion.li
+                key={link.name}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.8 }}
+              >
                 <Link to={link.path}>{link.name}</Link>
-              </li>
+              </motion.li>
             ))}
           </ul>
         </div>
-        <Link to="/cart" className="flex items-center gap-2">
-          <Icon name="Cart" /> <p>Cart</p>
-        </Link>
-      </div>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <Link to="/cart" className="flex items-center gap-2">
+            <Icon name="Cart" /> <p>Cart</p>
+          </Link>
+        </motion.div>
+      </motion.div>
       {hero ? (
         <div className="sm:w-_592 m-auto mt-_132 flex flex-col justify-center items-center gap-9">
-          <p className="text-4xl sm:text-_66 -tracking-_2 text-center font-semibold text-gray-200">
+          <motion.p
+            className="text-4xl sm:text-_66 -tracking-_2 text-center font-semibold text-gray-200"
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             Grow beautiful <br /> self-watering plants
-          </p>
-          <Button
-            label="Shop plants"
-            type="button"
-            onClick={handleClick}
-            className="w-60"
-          />
+          </motion.p>
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            <motion.div
+              whileHover={{
+                scale: 0.9,
+              }}
+              whileTap={{
+                scale: 0.8,
+              }}
+            >
+              <Button
+                label="Shop plants"
+                type="button"
+                onClick={handleClick}
+                className="w-60"
+              />
+            </motion.div>
+          </motion.div>
         </div>
       ) : null}
     </header>
