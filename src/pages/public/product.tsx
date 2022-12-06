02089ec99f3aabca468e04d7e31f35ed963580b7
Abou Daouda <50052665+Sall99@users.noreply.github.com>
2022-12-06 13:13:34 +0000

    feat(product): add mobile

--- src/pages/public/product.tsx
+++ src/pages/public/product.tsx
@@ -9,7 +9,7 @@ import { benefits } from "services";
 
 export const Benefits = () => {
   return (
-    <div className="flex flex-col gap-5 mt-10 text-gray-200 pb-10">
+    <div className="flex flex-col gap-5 mt-5 sm:mt-10 text-gray-200 pb-10">
       {benefits.map(({ img, title, description }, index) => (
         <div key={index} className="flex gap-5">
           <img src={img} alt={title} />
@@ -28,11 +28,11 @@ export const Product = () => {
   const plant = allPlants.find((plant) => plant.id === Number(id));
 
   return (
-    <div className="mt-_10 min-h-screen max-w-_1140 m-auto text-gray-200">
+    <div className="sm:mt-_10 min-h-screen max-w-_1140 m-auto text-gray-200">
       {plant ? (
         <div>
-          <div className="flex flex-col justify-center gap-10 md:flex-row">
-            <div className="w-1/3">
+          <div className="flex flex-col justify-center gap-5 sm:gap-10 md:flex-row">
+            <div className="sm:w-1/3">
               <Swiper modules={[Navigation]}>
                 {plant.images.map((image, index) => (
                   <SwiperSlide key={index}>
@@ -45,7 +45,7 @@ export const Product = () => {
                   </SwiperSlide>
                 ))}
               </Swiper>
-              <div className="grid grid-cols-6 gap-3">
+              <div className="hidden sm:grid grid-cols-6 gap-3">
                 {productGallery.map((image, index) => (
                   <img
                     key={index}
@@ -55,12 +55,12 @@ export const Product = () => {
                 ))}
               </div>
             </div>
-            <div className="w-_440">
+            <div className="sm:w-_440 px-5 sm:px-0">
               <h1 className="text-2xl font-bold">{plant.title}</h1>
               <p className="text-sm">{plant.description}</p>
               <p className="text-xl font-bold">${plant.price}</p>
-              <div className="mt-5">
-                <p className="text-base">Desert Brown</p>
+              <div className="mt-3 sm:mt-5">
+                <p className="text-base hidden sm:block">Desert Brown</p>
                 <div className="flex items-center-2 gap-2 mt-2">
                   {plant.colors.map((color, index) => {
                     return (
@@ -82,7 +82,10 @@ export const Product = () => {
               </div>
             </div>
           </div>
-          <Categories />
+          <div className="px-5">
+            <h2 className="text-gray-200 font-semibold">You may also like</h2>
+            <Categories />
+          </div>
         </div>
       ) : (
         <div className="text-center text-gray-200">
